53e1c1f7496d8d7111e7281543ac2610138a38b4
Luis Alejandro <luisalejandronietoruth@gmail.com>
2021-07-09 12:21:43 -0500

    LET y CONST, Multilínea, Spread Operator y Desestructuración

--- src/es6/index.js
+++ src/es6/index.js
@@ -18,3 +18,45 @@ let epicPhase = hello + ' ' + world;
 console.log(epicPhase);
 let epicPhase2 = `${hello} ${world}`;
 console.log(epicPhase2);
+
+
+let lorem = 'Esta es una frase epica \n'
++ 'Otra frase epica.';
+
+let lorem2 = `Otra frase mas epica
+ahora es otra linea epica`;
+
+console.log(lorem);
+console.log(lorem2);
+
+
+let persona = {
+    'name': 'Alex',
+    'age': 22,
+    'country': 'COl'
+}
+
+console.log(persona.name, persona.age);
+
+let { name, age } = persona;
+console.log(name, age);
+
+let team1 = ['Oscar', 'Alfred', 'Fred'];
+let team2 = ['Pedro', 'Maria', 'Lina'];
+
+let education = ['Alex', ...team1, ...team2];
+
+console.log(education);
+
+{
+    var globalvar = 'Global var'
+}
+
+{
+    let globalLet = 'Gobla let'
+    console.log(globalLet);
+}
+
+console.log(globalvar);
+
+const a = 'b';
