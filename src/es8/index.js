00853219218675c5798e10b378183e4f72ec6a87
Luis Alejandro <luisalejandronietoruth@gmail.com>
2021-07-12 11:36:40 -0500

    Async Await ES8

--- src/es8/index.js
+++ src/es8/index.js
@@ -29,3 +29,31 @@ console.log(values);
 const string = 'hello';
 console.log(string.padStart(8, 'hi '));
 console.log(string.padEnd(12, ' world'));
+
+
+// async Await
+const helloWorld = () => {
+    return new Promise((resolve, reject) => {
+        (true)
+        ? setTimeout(() => resolve('hello world'), 3000)
+        : reject(new Error('Error'))
+    })
+}
+
+const helloAsync = async () => {
+    const hello = await helloWorld();
+    console.log(hello);
+}
+
+helloAsync();
+
+const anotherFunction = async () => {
+    try {
+        const hello = await helloWorld();
+        console.log(hello);
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+anotherFunction();
